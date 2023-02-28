47270698a9fb8dd63856754586c88743bef7cb9f
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-28 21:53:27 +0300

    Add the script file for the projecr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 // import _ from 'lodash';
-import './style.css';
+// import './style.css';
+
+const clear = document.querySelector('.clear');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+const position = 'beforeend';
+
+const toDoList = [
+  {
+    index: 0,
+    description: 'Reading books',
+    completed: true,
+  },
+  {
+    index: 1,
+    description: 'Going for a walk',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Meet families',
+    completed: true,
+  },
+  {
+    index: 3,
+    description: 'play football',
+    completed: true,
+  },
+]
+
+function todo() {
+for(let i = 0; i < toDoList.length; i++){
+const item = `<li class="item">
+              <div class="left">
+              <i class="fa-regular fa-square"></i>
+              <p class="text">${toDoList[i].description}</p>
+              </div>
+              <i class="fa-solid fa-trash"></i>
+            </li>`
+            list.insertAdjacentHTML(position, item)
+}
+}
+
+window.document.addEventListener('DOMContentLoaded', todo);
 
 
 // function component() {
