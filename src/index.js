fe61779f8994b3b61729c7fc5a81273dd01bfef2
Amanuel Belete <amanuelbelete95@gmail.com>
2023-03-01 21:17:08 +0300

    Fix the linter errors

--- src/index.js
+++ src/index.js
@@ -2,37 +2,21 @@ const list = document.getElementById('list');
 const input = document.getElementById('input');
 const addBtn = document.querySelector('.add');
 
-let LIST ;
-let id ;
+let LIST;
+let id;
 
-const check = "fa-square-check";
-const uncheck = "fa-square";
-const line = "line-through";
+const check = 'fa-square-check';
+const uncheck = 'fa-square';
+const line = 'line-through';
 // local storage
-const loadList = array => {
-  array.forEach(item => {
-    addToDo(item.todo, item.id, item.done, item.trash)
-  });
-}
-let myData = localStorage.getItem("TODO");
-if(myData){
-  LIST = JSON.parse(myData);
-  id = LIST.length;
-  loadList(LIST);
-}
-else {
-  LIST = [];
-  id = 0;
-}
 
-function addToDo(todo, id, done,trash) {
+function addToDo(todo, id, done, trash) {
+  if (trash) { return; }
+  const Done = done ? check : uncheck;
+  const Line = done ? line : '';
+  const position = 'beforeend';
 
-  if(trash){ return;}
-const Done = done ? check: uncheck;
-const Line = done ? line: '';
-const position = 'beforeend';
-
-const item = `<li class="item">
+  const item = `<li class="item">
               <div class="left">
               <i class="fa-regular ${Done}" job="complete" id=${id}></i>
               <p class="text ${Line}">${todo}</p>
@@ -40,52 +24,64 @@ const item = `<li class="item">
               <i class="fa-solid fa-trash" job="delete" id=${id}></i>
             </li>`;
 
-            
-            list.insertAdjacentHTML(position, item)
+  list.insertAdjacentHTML(position, item);
+}
+
+const loadList = (array) => {
+  array.forEach((item) => {
+    addToDo(item.todo, item.id, item.done, item.trash);
+  });
+};
+const myData = localStorage.getItem('TODO');
+if (myData) {
+  LIST = JSON.parse(myData);
+  id = LIST.length;
+  loadList(LIST);
+} else {
+  LIST = [];
+  id = 0;
 }
 
 addBtn.addEventListener('click', () => {
   const todo = input.value;
 
-  if(todo){
+  if (todo) {
     addToDo(todo, id, false, false);
     LIST.push({
-     todo: todo,
-     id: id,
-     done: false,
-     trash: false
+      todo,
+      id,
+      done: false,
+      trash: false,
     });
 
     id += 1;
-    localStorage.setItem("TODO", JSON.stringify(LIST));
+    localStorage.setItem('TODO', JSON.stringify(LIST));
   }
 
-  input.value = "";
-})
+  input.value = '';
+});
 
-function completeToDo(element){
+function completeToDo(element) {
   element.classList.toggle(check);
   element.classList.toggle(uncheck);
   element.parentNode.querySelector('.text').classList.toggle(line);
-  LIST[element.id].done = LIST[element.id].done ? false: true;
+  LIST[element.id].done = !LIST[element.id].done;
 }
 
-function removeToDo(element){
-element.parentNode.parentNode.removeChild(element.parentNode);
-LIST[element.id].trash = true;
-
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
 }
 
-list.addEventListener('click', event => {
+list.addEventListener('click', (event) => {
   const element = event.target;
   const elementJob = element.attributes.job.value;
-  if(elementJob == 'complete'){
+  if (elementJob === 'complete') {
     completeToDo(element);
-  }
-  else if(elementJob == 'delete'){
+  } else if (elementJob === 'delete') {
     removeToDo(element);
   }
   localStorage.setItem('TODO', JSON.stringify(LIST));
 });
 
-document.addEventListener('DOMContentLoaded',loadList)
+document.addEventListener('DOMContentLoaded', loadList);
